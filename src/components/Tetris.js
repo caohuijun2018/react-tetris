d8ab241545155ae2c001a24f81d45a38948eef78
caohuijun2018 <1160364324@qq.com>
2021-04-09 21:30:03 +0800

    按键 左和右的位置变化

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,42 +1,66 @@
 import React, { useState } from "react";
 
+import { createStage } from "../gameHelper";
 //Compoments
 import Stage from "./Stage";
 import Display from "./Display";
 import StartButton from "./StartButton";
 //styled Components
-import {StyledTetrisWrapper ,StyledTetrise} from "./styles/StyledTetris";
-// import { createStage } from "../gameHelper";
+import { StyledTetrisWrapper, StyledTetrise } from "./styles/StyledTetris";
+
 //Custom Hooks
-import {useStage} from '../hooks/useStage';
+import { useStage } from "../hooks/useStage";
 import { usePlayer } from "../hooks/usePlayer";
 
-
 const Tetris = () => {
-const [dropTime,setDropTime] = useState();
-const [gameOver,setGameOver] =useState(false);
- 
+  const [dropTime, setDropTime] = useState();
+  const [gameOver, setGameOver] = useState(false);
 
-const [player] = usePlayer();  //pos,tetrominos
-const [stage,setStage] = useStage(player);
+  const [player,updataPlayerPos,resetPlayer] = usePlayer(); //pos,tetrominos
+  const [stage, setStage] = useStage(player);
 
-console.log('re-render')
+  console.log("re-render");
+  const movePlayer = (din) => {
+    updataPlayerPos({ x: din, y: 0 });
+  };
+  const startGame = () => {
+    setStage(createStage()); //重新构建stage
+    resetPlayer();
+  };
+  const drop = () => {
+    updataPlayerPos = { x: 0, y: 1, collided: false };
+  };
+  const dropPlayer = () => {
+    drop();
+  };
+  const move = ({ keyCode }) => {
+    if (!gameOver) {
+      if (keyCode === 37) {
+        //左移动
+        movePlayer(-1);
+      } else if (keyCode === 39) {
+        //右移
+        movePlayer(1);
+      } else if (keyCode === 40) {
+        dropPlayer();
+      }
+    }
+  };
   return (
-    <StyledTetrisWrapper>
+    <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={(e) => move(e)}>
       <StyledTetrise>
         <Stage stage={stage} />
         <aside>
           {gameOver ? (
-            <Display gameOver = {gameOver}/>
+            <Display gameOver={gameOver} />
           ) : (
-          <div>
-            <Display text="Score" />
-            <Display text="Rows" />
-            <Display text="Level" />
-          </div>
-           )}
-          <StartButton />
-         
+            <div>
+              <Display text="Score" />
+              <Display text="Rows" />
+              <Display text="Level" />
+            </div>
+          )}
+          <StartButton onClick={startGame} />
         </aside>
       </StyledTetrise>
     </StyledTetrisWrapper>
--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,4 +1,5 @@
-import {useState} from 'react'
+import {useCallback, useState} from 'react'
+import { STAGE_WIDTH } from '../gameHelper';
 import {randomTetromions} from '../tetrominos'
 
 export const usePlayer = () => {
@@ -7,7 +8,21 @@ export const usePlayer = () => {
         tetromino: randomTetromions().shape,
         collided: false  //碰撞属性
     });
+    const updataPlayerPos = ({x,y,collided}) => {
+        setPlayer(prev => ({
+            ...prev,
+            pos : {x : (prev.pos.x += x), y: (prev.pos.y += y)},
+            collided
+        }))
+    }
+    const resetPlayer = useCallback( () => {
+        setPlayer({
+            pos : {x : STAGE_WIDTH / 2 - 2 , y : 0},
+            tetromino:randomTetromions().shape,
+            collided : false
+        })
+    })
 
-    return [player];
+    return [player,updataPlayerPos,resetPlayer];
 
 }
