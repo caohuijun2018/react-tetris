5801a8ca9552f146e7789c719cddc7fcfda295fe
caohuijun2018 <1160364324@qq.com>
2021-04-10 17:28:45 +0800

    修改判断条件

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -3,32 +3,36 @@ import { useEffect, useState } from "react";
 import { createStage } from "../gameHelper";
 
 export const useStage = (player, resetPlayer) => {
-  const [stage, setStage] = useState(createStage()); //棋盘
-    
-  useEffect(() => {
+    const [stage, setStage] = useState(createStage());
+  
+    useEffect(() => {
       const updateStage = prevStage => {
-          //flush the stage
-          const newStage = prevStage.map(row => 
-                row.map(cell => (cell[1] === 'clear' ? [0,'clear']: cell))
-            )
-
-            //draw the tetimino
-            player.tetromino.forEach((row,y) => {
-                row.forEach((value,x) => {
-                    if(value !== 0) {
-                        newStage[y + player.pos.y][x + player.pos.x] = [
-                            value,
-                            `${player.collided ? 'merged' : 'clear'},`
-                        ]
-                    }
-                })
-            });
-            console.log('newstage',)
-            return newStage;
-      }
-
-      setStage(prev => updateStage(prev))
-  }, [player,resetPlayer]);
-
-  return [stage, setStage];
-};
+        // First flush the stage
+        const newStage = prevStage.map(row =>
+          row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell)),
+        );
+  
+        // Then draw the tetromino
+        player.tetromino.forEach((row, y) => {
+          row.forEach((value, x) => {
+            if (value !== 0) {
+              newStage[y + player.pos.y][x + player.pos.x] = [
+                value,
+                `${player.collided ? 'merged' : 'clear'}`,
+              ];
+            }
+          });
+        });
+        // Then check if we collided
+        if (player.collided) {
+          resetPlayer();
+        }
+  
+        return newStage;
+      };
+  
+      setStage(prev => updateStage(prev));
+    }, [player, resetPlayer]);
+  
+    return [stage, setStage];
+  };
