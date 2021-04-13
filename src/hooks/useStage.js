eb5a6f92de43012d67a9b6d24945a80c077a87f5
caohuijun2018 <1160364324@qq.com>
2021-04-13 17:43:59 +0800

    添加满一行之后的清除动作

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -2,14 +2,27 @@ import { useEffect, useState } from "react";
 
 import { createStage } from "../gameHelper";
 
-export const useStage = (player,resetPlayer) => {
+export const useStage = (player, resetPlayer) => {
   const [stage, setStage] = useState(createStage());
-
+  const [rowsCleaded, setRowsCleaded] = useState(0);
   useEffect(() => {
-    const updateStage = prevStage => {
+    setRowsCleaded(0);
+
+    const sweepRows = (newStage) =>
+      newStage.reduce((ack, row) => {
+        if (row.findIndex((cell) => cell[0] === 0) === -1) {
+          setRowsCleaded((prev) => prev + 1);
+          ack.unshift(new Array(newStage[0].length).fill([0, "clear"])); //用压入一行新的空数组的方式，代替所有的数组向下移动
+          return ack;
+        }
+        ack.push(row);
+        return ack;
+      }, []);
+
+    const updateStage = (prevStage) => {
       // First flush the stage
-      const newStage = prevStage.map(row =>
-        row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell)),
+      const newStage = prevStage.map((row) =>
+        row.map((cell) => (cell[1] === "clear" ? [0, "clear"] : cell))
       );
 
       // Then draw the tetromino
@@ -18,21 +31,23 @@ export const useStage = (player,resetPlayer) => {
           if (value !== 0) {
             newStage[y + player.pos.y][x + player.pos.x] = [
               value,
-              `${player.collided ? 'merged' : 'clear'}`,
+              `${player.collided ? "merged" : "clear"}`,
             ];
           }
         });
-      });//？？
+      }); //？？
+
       // Then check if we collided
       if (player.collided) {
         resetPlayer();
+        return sweepRows(newStage)
       }
 
       return newStage;
     };
 
-    setStage(prev => updateStage(prev));
+    setStage((prev) => updateStage(prev));
   }, [player, resetPlayer]);
 
-  return [stage, setStage];
+  return [stage, setStage,rowsCleaded];
 };
