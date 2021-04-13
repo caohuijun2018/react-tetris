68f4fa61753c76b0338193a2776fa917ba5426a6
caohuijun2018 <1160364324@qq.com>
2021-04-13 19:05:17 +0800

    添加score，若饿死，level显示，以及根据目前游戏进度，调整游戏难度

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -11,13 +11,17 @@ import { StyledTetrisWrapper, StyledTetrise } from "./styles/StyledTetris";
 //Custom Hooks
 import { useStage } from "../hooks/useStage";
 import { usePlayer } from "../hooks/usePlayer";
-import { useInterval} from '../hooks/useInterval'
+import { useInterval } from "../hooks/useInterval";
+import { useGameStatus } from "../hooks/useGameStatus";
 const Tetris = () => {
   const [dropTime, setDropTime] = useState();
   const [gameOver, setGameOver] = useState(false);
 
-  const [player, updataPlayerPos, resetPlayer,playerRotate] = usePlayer(); //pos,tetrominos
-  const [stage, setStage] = useStage(player,resetPlayer);
+  const [player, updataPlayerPos, resetPlayer, playerRotate] = usePlayer(); //pos,tetrominos
+  const [stage, setStage, rowsCleared] = useStage(player, resetPlayer);
+  const [score, setScore, rows, setRows, level, setLevel] = useGameStatus(
+    rowsCleared
+  );
 
   console.log("re-render");
   const movePlayer = (din) => {
@@ -28,16 +32,25 @@ const Tetris = () => {
   const startGame = () => {
     setStage(createStage()); //重新构建stage
     resetPlayer();
-    setGameOver(false)
+    setGameOver(false);
     setDropTime(1000);
+    setScore(0);
+    setLevel(0);
+    setRows(0);
   };
+
   const drop = () => {
+    if (rows > (level + 1) * 3) {
+      setLevel((prev) => prev + 1);
+
+      setDropTime(1000 / (level + 1) + 200);
+    }
     if (!checkCollision(player, stage, { x: 0, y: 1 })) {
       updataPlayerPos({ x: 0, y: 1, collided: false });
     } else {
-      if(player.pos.y < 1){
-        alert('Game Over!')
-        console.log('Game Over!')
+      if (player.pos.y < 1) {
+        alert("Game Over!");
+        console.log("Game Over!");
         setGameOver(true);
         setDropTime(null);
       }
@@ -48,13 +61,17 @@ const Tetris = () => {
     drop();
   };
 
-  const keyup = ({keyCode}) => { //松开上键时，恢复自动下降
-    if(!gameOver){
-      if(keyCode === 40){
+  const keyup = ({ keyCode }) => {
+    //松开上键时，恢复自动下降
+    if (!gameOver) {
+      if (keyCode === 40) {
         setDropTime(1000);
       }
     }
-  }
+  };
+  useInterval(() => {
+    drop();
+  }, [dropTime]);
   const move = ({ keyCode }) => {
     if (!gameOver) {
       if (keyCode === 37) {
@@ -65,18 +82,21 @@ const Tetris = () => {
         movePlayer(1);
       } else if (keyCode === 40) {
         dropPlayer();
-        setDropTime(null); //当用户按住下键时，取消自动下降
-      }else if(keyCode === 38){
-        console.log('上键')
-        playerRotate(stage,1);
+        setDropTime(300); //当用户按住下键时，取消自动下降
+      } else if (keyCode === 38) {
+        console.log("上键");
+        playerRotate(stage, 1);
       }
     }
   };
-  useInterval(() => {
-    drop();
-  },[dropTime])
+
   return (
-    <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={(e) => move(e)} onKeyUp = {keyup}>
+    <StyledTetrisWrapper
+      role="button"
+      tabIndex="0"
+      onKeyDown={(e) => move(e)}
+      onKeyUp={keyup}
+    >
       <StyledTetrise>
         <Stage stage={stage} />
         <aside>
@@ -84,9 +104,9 @@ const Tetris = () => {
             <Display gameOver={gameOver} />
           ) : (
             <div>
-              <Display text="Score" />
-              <Display text="Rows" />
-              <Display text="Level" />
+              <Display text={`Scorce : ${score}`} />
+              <Display text={`Rows : ${rows}`} />
+              <Display text={`Level : ${level}`} />
             </div>
           )}
           <StartButton onClick={startGame} />
