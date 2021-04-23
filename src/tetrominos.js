1fa2479e74cd9d42c2a892c533c0f15f44c0389e
caohuijun2018 <1160364324@qq.com>
2021-04-23 21:27:37 +0800

    更新项目的背景，排版

--- src/tetrominos.js
+++ src/tetrominos.js
@@ -10,7 +10,7 @@ export  const TETROMINOS = {
       [0, "I", 0, 0],
       [0, "I", 0, 0],
     ],
-    color: "80,223,230",
+    color: "	0, 191, 255",
   },
   J: {
     shape: [
@@ -18,7 +18,7 @@ export  const TETROMINOS = {
       [0, "J", 0],
       ["J", "J", 0],
     ],
-    color: "80,223,230",
+    color: "255, 191, 134",
   },
   L: {
     shape: [
@@ -26,14 +26,14 @@ export  const TETROMINOS = {
       [0, "L", 0],
       [0, "L", "L"],
     ],
-    color: "223,173,36",
+    color: "144, 238, 144",
   },
   O: {
     shape: [
       ["O", "O"],
       ["O", "O"],
     ],
-    color: "223,173,36",
+    color: "147, 112, 219",
   },
   S: {
     shape: [
@@ -49,7 +49,7 @@ export  const TETROMINOS = {
       ["T", "T", "T"],
       [0, "T", 0],
     ],
-    color: "132,61,198",
+    color: "255, 193, 193",
   },
   Z: {
     shape: [
@@ -57,7 +57,7 @@ export  const TETROMINOS = {
       [0, "Z", "Z"],
       [0, 0, 0],
     ],
-    color: "223,78,78",
+    color: "255, 128, 0",
   },
 };
  export const randomTetromions = () => {
